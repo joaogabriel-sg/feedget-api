38b61d3e1b24437561265c1a9ea7d104d64d7100
João Gabriel <main.joaogabrielsg@gmail.com>
2022-05-06 23:18:39 -0300

    feat: add port env variable

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+app.listen(process.env.PORT || 3333, () =>
+  console.log(`Server is running on port ${process.env.PORT || 3333}`)
+);
